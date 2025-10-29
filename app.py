f3f2e3b4918af2346e805a2e99775b8b3f977452
Rafella Dias <rafaell4.dias.g@gmail.com>
2025-10-28 22:28:55 -0300

    Adicionando controle de sessões

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from database import executar_comandos
 from mapas import gerar_mapa
 import bcrypt
@@ -6,7 +7,31 @@ import bcrypt
 app = Flask(__name__)
 app.secret_key = 'chave_muito_secreta'
 
-#rota de 
+# Configuração Flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login_submit'
+
+class User(UserMixin):
+    def __init__(self, id, nome, email, senha):
+        self.id = id
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+@login_manager.user_loader
+def load_user(user_id):
+    query = "SELECT id, nome_usuario, email FROM usuarios WHERE id = %s"
+    valores = user_id
+    resultado = executar_comandos(query, valores, fetchone = True, retornar_id = False)
+
+    if resultado:
+        id, nome_usuario, email = resultado
+        return User(id, nome_usuario, email)
+
+    return None
+
+#Rotas
 @app.route('/')
 def principal():
     return render_template('index.html')
@@ -48,28 +73,46 @@ def login_submit():
     if request.method == 'POST':
         email = request.form.get('email')
         senha = request.form.get('senha')
-        '''Gerando a query  para fazer o login'''
+
+        #Gerando a query  para fazer o login
         query = "SELECT senha_hash, id FROM usuarios WHERE email = %s"
         valores = (email,)
-
         resultado = executar_comandos(query, valores,fetchone=True, retornar_id=False)            
-        
 
         if resultado:
-            password = resultado[0]
-            session['usuario_id'] = resultado[1]
-            if bcrypt.checkpw(senha.encode('utf-8'), password.encode('utf-8')):
-                return redirect(url_for('inicio'))
-            
-            mensagem = "Erro! Digite os campos novamente!"
-            return render_template('login.html',mensagem = mensagem)
+            id_usuario, nome_usuario, senha_hash = resultado
+
+            if bcrypt.checkpw(senha.encode('utf-8'), senha_hash.encode('utf-8')):
+                user = User(id_usuario, nome_usuario, email)
+                login_user = user
+
+                session['nome'] = nome_usuario
+
+                return redirect(url_for('home'))
+            else:
+                mensagem = 'Senha incorreta!'
+                return render_template('login.html', mensagem = mensagem)
         
         else:
-            mensagem = "Login Inválido!"
+            mensagem = "Email não cadastrado!"
             return render_template('login.html',mensagem = mensagem)    
+        
     return  render_template('login.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    session.pop('nome', None)
+    return redirect(url_for('login_submit'))
+
+@app.route('/home',methods=['GET'])
+#@login_required
+def inicio():
+    return render_template('home.html')
+
 @app.route('/perfil', methods=['GET', 'POST'])
+#@login_required
 def perfil():
     usuario_id = session['usuario_id']
     query = '''SELECT nome_usuario FROM usuarios WHERE id = %s'''
@@ -82,18 +125,15 @@ def perfil():
 
     return render_template('perfil.html', nome_usuario = nome_usuario)
 
-
-@app.route('/home',methods=['GET'])
-def inicio():
-    return render_template('home.html')
-
 @app.route('/mapa/academia')
+#@login_required
 def mapa_view_academia():
     mapa_html = gerar_mapa('academia')
     nome = session.get('nome', 'Visitante')
     return render_template('mapa.html', nome=nome, mapa_html=mapa_html)
 
 @app.route('/mapa/lojas')
+#@login_required
 def mapa_view_loja():
     mapa_html = gerar_mapa('lojas')
     nome = session.get('nome', 'Visitante')
