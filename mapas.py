238fc67358f390df0d8182f60066bfe0a623a7ee
João Victor <joaovictorqueimadas@gmail.com>
2025-11-12 23:54:49 -0300

    ajustando mapa

--- mapas.py
+++ mapas.py
@@ -29,6 +29,13 @@ def gerar_mapa(opcao):
         width='100vw',
         height='100vw'
     )
+    folium.TileLayer(
+        tiles='https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=ibuk3ZTj3Ebly6X9wlX6W9Hjql0bxNrZ3eXvdmIvQFXeAjPZtX00h3J6kRJwQVWG',
+        attr='<a href="https://jawg.io" target="_blank">&copy; Jawg Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        name='Black').add_to(mapa_local)
+
+    folium.LayerControl().add_to(mapa_local)
+
     LocateControl(auto_start=True, drawmarker=True, markerStyle = dict(classname="leaflet-control-locate-marker")).add_to(mapa_local)
     Terminator().add_to(mapa_local)
     if opcao in (None, '', '0'):
@@ -42,6 +49,16 @@ def gerar_mapa(opcao):
                 popup=nome,
                 icon=folium.Icon(icon='dumbbell', prefix='fa', color='orange')  
             ).add_to(mapa_local)
+        for academia_livre in coordenadas_academias_livres:
+            lat = academia_livre[0]
+            lon = academia_livre[1]
+            nome = academia_livre[2]
+            coord = [lat, lon]
+            folium.Marker(
+                location=coord,
+                popup=nome,
+                icon=folium.Icon(icon='dumbbell', prefix='fa', color='blue')  
+            ).add_to(mapa_local)
         for quadra in coordenadas_quadras:
             lat = quadra[0]
             lon = quadra[1]
@@ -50,7 +67,7 @@ def gerar_mapa(opcao):
             folium.Marker(
                 location=coord,
                 popup=nome,
-                icon=folium.Icon(icon='shopping-cart', prefix='fa', color='green')  
+                icon=folium.Icon(icon_image='\static\icons\person-arms-up.svg',color='green', icon_color='white')  
             ).add_to(mapa_local)
         for crosfits in coordenadas_crossfits:
             lat = crosfits[0]
@@ -60,7 +77,7 @@ def gerar_mapa(opcao):
             folium.Marker(
                 location=coord,
                 popup=nome,
-                icon=folium.Icon(icon='ball', prefix='fa', color='black')  
+                icon=folium.Icon(icon='glyphicon glyphicon-flash', prefix='glyphicon', color='black', icon_color='white')  
             ).add_to(mapa_local)
         for lojas in coordenadas_lojas:
             lat = lojas[0]
@@ -95,7 +112,7 @@ def gerar_mapa(opcao):
             folium.Marker(
                 location=coord,
                 popup=nome,
-                icon=folium.Icon(icon='dumbbell', prefix='fa', color='orange')  
+                icon=folium.Icon(icon='dumbbell', prefix='fa', color='blue')  
             ).add_to(mapa_local)
 
     if opcao == '3':
@@ -107,7 +124,7 @@ def gerar_mapa(opcao):
             folium.Marker(
                 location=coord,
                 popup=nome,
-                icon=folium.Icon(icon='ball', prefix='fa', color='green')  
+                icon=folium.Icon(icon='glyphicon glyphicon-fire',color='green', icon_color='white')  
             ).add_to(mapa_local)
     if opcao == '4':
          for crosfits in coordenadas_crossfits:
@@ -118,7 +135,7 @@ def gerar_mapa(opcao):
             folium.Marker(
                 location=coord,
                 popup=nome,
-                icon=folium.Icon(icon='ball', prefix='fa', color='black')  
+                icon=folium.Icon(icon='glyphicon glyphicon-flash', prefix='glyphicon', color='black', icon_color='white') 
             ).add_to(mapa_local)
 
     elif opcao == '5':
